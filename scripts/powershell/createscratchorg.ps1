d137eb2cab8bd184d1a4110f419d425478d4022f
AMAN SAWHNEY <sawhneyaman3@gmail.com>
2023-11-07 19:02:07 +0530

    Scratch Org Creation Script

--- scripts/powershell/createscratchorg.ps1
+++ scripts/powershell/createscratchorg.ps1
@@ -1,19 +1,16 @@
-# Run the Salesforce CLI command to list orgs and capture the JSON output
-$sfdxOrgListOutput = sf org list --verbose --json
-
 # Parse the JSON output
-$orgList = $sfdxOrgListOutput | ConvertFrom-Json
+$orgList = sf org list --verbose --json | ConvertFrom-Json
 
 # Find the default Dev Hub alias from the JSON
 $defaultDevHubAlias = $orgList.result.devHubs | Where-Object { $_.connectedStatus } | Select-Object -ExpandProperty alias
 
 # Check if the Dev Hub is not authenticated
 if (-not $defaultDevHubAlias) {
-    Write-Host "Default Dev Hub is not authenticated. Please authenticate your Dev Hub first and rerun the script."
+    Write-Host "Default Dev Hub is not authenticated. Please authenticate your Dev Hub first and rerun the script." -BackgroundColor red -ForegroundColor white
     exit 1 # Exit the script with an error code
 }
 else{
-    Write-Host "Default Dev Hub is authenticated."
+    Write-Host "Default Dev Hub is authenticated. Proceeding to create a scratch org with dev hub having alias - "$defaultDevHubAlias  -BackgroundColor green -ForegroundColor white
 
     # Generate a unique scratch org alias based on email format and current date-time
     $email = Read-Host -Prompt "Enter your email address (e.g., your.email@example.com)"
@@ -23,18 +20,26 @@ else{
     $githubBranch = (git rev-parse --abbrev-ref HEAD)
 
     # Create a Salesforce scratch org
-    sf org create scratch --set-default --definition-file config/project-scratch-def.json -a $scratchOrgAlias --duration-days 30 --admin-email $email 
+    $scratchOrgErrorCode = sf org create scratch --set-default --definition-file config/project-scratch-def.json -a $scratchOrgAlias --duration-days 30 --admin-email $email 
+    if($?){
+        # Install nebula Logger
+        Write-Host "Installing Nebula Logger"
+        sf package install -w 60 --security-type AdminsOnly --package 04t5Y000001Mjx5QAC  --no-prompt
 
-    # Install nebula Logger
-    sf package install -w 60 --security-type AdminsOnly --package 04t5Y000001Mjx5QAC
+        # Push the code to the scratch org
+        sf project deploy start -o $scratchOrgAlias -w 60
 
-    # Push the code to the scratch org
-    sf project deploy start -o $scratchOrgAlias -w 60
+        # Assign the default user in the scratch org to this permission set
+        sf org assign permset --name GIFter
+        sf org assign permset --name LoggerAdmin
 
-    # Assign the default user in the scratch org to this permission set
-    sf org assign permset --name GIFter
-    sf org assign permset --name LoggerAdmin
+        # open the scratch org
+        sf org open --path lightning/n/GIFter
 
-    # open the scratch org
-    sf org open --path lightning/n/GIFter
+        # import data
+        #sf data import tree --plan importable-data/Account-Contact-Opportunity-plan.json
+    }
+    else{
+        Write-Host "Scratch Org cannot be created." -BackgroundColor red -ForegroundColor white
+    }
 }
